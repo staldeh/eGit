2518a2129411cd788ae9f1048ecca3e32525e88d
Stefan Albinsson <stefan.albinsson@gmail.com>
2013-06-16 12:31:12 +0200

    Added !

--- Hello/src/Hello.cpp
+++ Hello/src/Hello.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!!" << endl; // prints !!!Hello World!!!
 
 	return 0;
 }
